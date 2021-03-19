ea2baa865494cda5ade9d73e6aeb787a65034b5f
Randy Ortiz <Randyortiz28@gmail.com>
2021-03-19 11:32:29 -0400

    Connected my reducer and created my fetch data actions for start, success, and error

--- src/components/SmurfList.js
+++ src/components/SmurfList.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
 import Smurf from './Smurf';
+import { fetchSmurfData } from '../actions/'
 
  const SmurfList = ()=> {
-    const isLoading = false;
-    const testSmurf = {
-        id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
-        name:'Poppa Smurf',
-        position:'Village Leader',
-        nickname: 'Pops',
-        description: 'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.'
-    }
+
+    const {smurfs, isLoading } = useSelector(state => state)
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchSmurfData())
+    },[dispatch])
+    
 
     if (isLoading) {
         return <h1>Loading...</h1>;
     }
 
-    return(<div className="listContainer">
-        <Smurf smurf={testSmurf}/>
+    return(
+    <div className="listContainer">
+        {smurfs.map(smurf => {
+            return (
+                <Smurf smurf={smurf}/>
+            )
+        })}
     </div>);
 }
 
